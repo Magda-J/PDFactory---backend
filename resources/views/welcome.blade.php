935d9861cf5587fcdcf45868f75aff0471383812
Magda-J <magdalena.jakonska@gmail.com>
2024-04-11 10:31:44 +0100

    Styling added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite('resource/css/app.css')
+        @vite('resources/css/app.css')
     </head>
     <body>
        <div id="app"></div>
