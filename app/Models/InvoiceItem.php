c5f1ad80c8c5e056f023cedeb05d594dfd392c2e
Magda-J <magdalena.jakonska@gmail.com>
2024-04-17 14:22:15 +0100

    Create new invoice is ready

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class InvoiceItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'product_id',
+      'invoice_id',
+      'quantity',
+      'unit_price'
+
+
+    ];
 }
